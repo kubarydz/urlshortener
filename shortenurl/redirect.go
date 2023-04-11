9d39b1463732c117d7587cf6304b913a7e2b2caf
Jakub Rydz <niechzyjetequila@gmail.com>
2023-04-11 17:23:46 +0200

    rm garbage

--- shortenurl/redirect.go
+++ shortenurl/redirect.go
@@ -4,12 +4,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type URL struct {
-	ID        uint   `gorm:"primary_key"`
-	Original  string `gorm:"not null"`
-	Shortened string `gorm:"not null"`
-}
-
 func RedirectURL(c *fiber.Ctx) error {
 	url := c.Params("url")
 	db := Connect()
